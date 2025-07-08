954755cbc97aa75a68d48d661fc26b83c3911fec
Nikunjkanteliya <kanteliyanikunj422@gmail.com>
2025-07-08 18:00:00 +0530

    code added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 "use client";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import Header from "./components/Header";
 import Body from "./components/Body";
 export default function Home() {
-  const router = useRouter();
   return (
     <div>
       <Body />
