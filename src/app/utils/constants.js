37c8ff41c7a080780bce0582b84b632f6c31ad33
Nikunjkanteliya <kanteliyanikunj422@gmail.com>
2025-07-16 15:32:51 +0530

    arrows added

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -32,8 +32,8 @@ export const settings = {
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
+        dots: false,
+        arrows: true,
       },
     },
   ],
