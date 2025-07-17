da5033afa4565c67a618e42246a53f0270d6d4c2
Nikunjkanteliya <kanteliyanikunj422@gmail.com>
2025-07-17 18:16:12 +0530

    eye added

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useRef, useState } from "react";
+import { FaRegEye, FaEyeSlash } from "react-icons/fa";
 import Header from "./Header";
 import { formValidation } from "../utils/validation";
 import {
@@ -15,11 +16,10 @@ import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [issignin, setIssignin] = useState(true);
-  // const [issignin, setIssignin] = useState(true);
+  const [isShowpassword, setIsShowpassword] = useState(false);
 
   const router = useRouter();
   const dispatch = useDispatch();
-
 
   const [error, setError] = useState();
 
@@ -48,7 +48,7 @@ const Login = () => {
           .then((userCredential) => {
             // Signed up
             const user = auth?.currentUser;
-          
+
             toast.success("Account created successfully");
 
             updateProfile(auth.currentUser, {
@@ -58,7 +58,7 @@ const Login = () => {
             })
               .then(() => {
                 // Profile updated!
-                
+
                 dispatch(
                   addUser({
                     uid: user?.uid,
@@ -107,6 +107,7 @@ const Login = () => {
       }
     }
   };
+
   return (
     <div className="relative min-h-screen w-full">
       <div className="absolute inset-0 bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_medium.jpg')] bg-cover bg-center opacity-50 -z-10"></div>
@@ -114,6 +115,7 @@ const Login = () => {
       <div className="mx-auto max-w-[480px] text-white bg-black/70  px-16 py-12  rounded-md">
         <h1 className="text-3xl">{issignin ? "Sign In" : "Sign Up"}</h1>
         <form
+          className="relative"
           onSubmit={(e) => {
             e.preventDefault();
           }}
@@ -148,13 +150,24 @@ const Login = () => {
           <>
             <input
               ref={password}
-              type="password"
+              type={isShowpassword ? "password" : "text"}
               placeholder="password"
-              className="w-full mt-4 px-4 py-3 rounded-md border-[1px]"
+              className="w-full mt-4 px-4 py-3 rounded-md border-[1px] "
             />
             <p className="text-red-700 font-bold text-[12px] p-1">
               {error?.passwordError}
             </p>
+
+            <div
+              className="absolute top-[105px] right-[20px] "
+              onClick={() => setIsShowpassword(!isShowpassword)}
+            >
+              {isShowpassword ? (
+                <FaRegEye className="w-[25px]" />
+              ) : (
+                <FaEyeSlash className="w-[25px]" />
+              )}
+            </div>
           </>
           <button
             onClick={handleSigninValidation}
--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -2,7 +2,7 @@ const NextArrow = (props) => {
   const { className, style, onClick } = props;
   return (
     <div
-      className={`${className} custom-arrow next`}
+      className={` custom-arrow next`}
       style={{ ...style, display: "block" }}
       onClick={onClick}
     >
@@ -131,5 +131,48 @@ export const selectArrow = (
       strokeLinecap="round"
       strokeLinejoin="round"
     />
+  </svg>
+);
+
+export const hidePassword = (
+  <svg
+    width="30px"
+    height="30px"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M2 2L22 22"
+      stroke="#000000"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6.71277 6.7226C3.66479 8.79527 2 12 2 12C2 12 5.63636 19 12 19C14.0503 19 15.8174 18.2734 17.2711 17.2884M11 5.05822C11.3254 5.02013 11.6588 5 12 5C18.3636 5 22 12 22 12C22 12 21.3082 13.3317 20 14.8335"
+      stroke="#000000"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M14 14.2362C13.4692 14.7112 12.7684 15.0001 12 15.0001C10.3431 15.0001 9 13.657 9 12.0001C9 11.1764 9.33193 10.4303 9.86932 9.88818"
+      stroke="#000000"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+export const showPassword = (
+  <svg
+    fill="#000000"
+    width="30px"
+    height="30px"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M21.92,11.6C19.9,6.91,16.1,4,12,4S4.1,6.91,2.08,11.6a1,1,0,0,0,0,.8C4.1,17.09,7.9,20,12,20s7.9-2.91,9.92-7.6A1,1,0,0,0,21.92,11.6ZM12,18c-3.17,0-6.17-2.29-7.9-6C5.83,8.29,8.83,6,12,6s6.17,2.29,7.9,6C18.17,15.71,15.17,18,12,18ZM12,8a4,4,0,1,0,4,4A4,4,0,0,0,12,8Zm0,6a2,2,0,1,1,2-2A2,2,0,0,1,12,14Z" />
   </svg>
 );
